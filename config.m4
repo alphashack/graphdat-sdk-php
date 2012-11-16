9f206a21412c1c049554b7253f92b860ce22b890
Sugendran Ganess <sugendran@sugendran.net>
2012-11-16 14:25:25 +1100

    Initial skeleton made by the ext_skel script

---
+++ config.m4
@@ -0,0 +1,63 @@
+dnl $Id$
+dnl config.m4 for extension graphdat
+
+dnl Comments in this file start with the string 'dnl'.
+dnl Remove where necessary. This file will not work
+dnl without editing.
+
+dnl If your extension references something external, use with:
+
+dnl PHP_ARG_WITH(graphdat, for graphdat support,
+dnl Make sure that the comment is aligned:
+dnl [  --with-graphdat             Include graphdat support])
+
+dnl Otherwise use enable:
+
+dnl PHP_ARG_ENABLE(graphdat, whether to enable graphdat support,
+dnl Make sure that the comment is aligned:
+dnl [  --enable-graphdat           Enable graphdat support])
+
+if test "$PHP_GRAPHDAT" != "no"; then
+  dnl Write more examples of tests here...
+
+  dnl # --with-graphdat -> check with-path
+  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
+  dnl SEARCH_FOR="/include/graphdat.h"  # you most likely want to change this
+  dnl if test -r $PHP_GRAPHDAT/$SEARCH_FOR; then # path given as parameter
+  dnl   GRAPHDAT_DIR=$PHP_GRAPHDAT
+  dnl else # search default path list
+  dnl   AC_MSG_CHECKING([for graphdat files in default path])
+  dnl   for i in $SEARCH_PATH ; do
+  dnl     if test -r $i/$SEARCH_FOR; then
+  dnl       GRAPHDAT_DIR=$i
+  dnl       AC_MSG_RESULT(found in $i)
+  dnl     fi
+  dnl   done
+  dnl fi
+  dnl
+  dnl if test -z "$GRAPHDAT_DIR"; then
+  dnl   AC_MSG_RESULT([not found])
+  dnl   AC_MSG_ERROR([Please reinstall the graphdat distribution])
+  dnl fi
+
+  dnl # --with-graphdat -> add include path
+  dnl PHP_ADD_INCLUDE($GRAPHDAT_DIR/include)
+
+  dnl # --with-graphdat -> check for lib and symbol presence
+  dnl LIBNAME=graphdat # you may want to change this
+  dnl LIBSYMBOL=graphdat # you most likely want to change this 
+
+  dnl PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
+  dnl [
+  dnl   PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $GRAPHDAT_DIR/lib, GRAPHDAT_SHARED_LIBADD)
+  dnl   AC_DEFINE(HAVE_GRAPHDATLIB,1,[ ])
+  dnl ],[
+  dnl   AC_MSG_ERROR([wrong graphdat lib version or lib not found])
+  dnl ],[
+  dnl   -L$GRAPHDAT_DIR/lib -lm
+  dnl ])
+  dnl
+  dnl PHP_SUBST(GRAPHDAT_SHARED_LIBADD)
+
+  PHP_NEW_EXTENSION(graphdat, graphdat.c, $ext_shared)
+fi
---
+++ php_graphdat.h
@@ -0,0 +1,83 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2012 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef PHP_GRAPHDAT_H
+#define PHP_GRAPHDAT_H
+
+extern zend_module_entry graphdat_module_entry;
+#define phpext_graphdat_ptr &graphdat_module_entry
+
+#ifdef PHP_WIN32
+#	define PHP_GRAPHDAT_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_GRAPHDAT_API __attribute__ ((visibility("default")))
+#else
+#	define PHP_GRAPHDAT_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(graphdat);
+PHP_MSHUTDOWN_FUNCTION(graphdat);
+PHP_RINIT_FUNCTION(graphdat);
+PHP_RSHUTDOWN_FUNCTION(graphdat);
+PHP_MINFO_FUNCTION(graphdat);
+
+PHP_FUNCTION(confirm_graphdat_compiled);	/* For testing, remove later. */
+
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+
+ZEND_BEGIN_MODULE_GLOBALS(graphdat)
+	long  global_value;
+	char *global_string;
+ZEND_END_MODULE_GLOBALS(graphdat)
+*/
+
+/* In every utility function you add that needs to use variables 
+   in php_graphdat_globals, call TSRMLS_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMLS_CC
+   after the last function argument and declare your utility function
+   with TSRMLS_DC after the last declared argument.  Always refer to
+   the globals in your function as GRAPHDAT_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define GRAPHDAT_G(v) TSRMG(graphdat_globals_id, zend_graphdat_globals *, v)
+#else
+#define GRAPHDAT_G(v) (graphdat_globals.v)
+#endif
+
+#endif	/* PHP_GRAPHDAT_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
