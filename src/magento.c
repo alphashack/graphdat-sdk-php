b722eee6244986f51beb674f774d9f095e72bc38
Sugendran Ganess <sugendran@sugendran.net>
2012-12-04 17:41:29 +1100

    magento

--- src/magento.c
+++ src/magento.c
@@ -14,11 +14,9 @@
 
 int hasMagento(TSRMLS_D)
 {
-	if(zend_hash_exists(EG(class_table), "mage", sizeof("mage")))
+	if(zend_hash_exists(EG(class_table), "mage", sizeof("mage")) 
+		&& zend_hash_exists(EG(class_table), "mage_core_controller_varien_action", sizeof("mage_core_controller_varien_action")) )
 	{
-	    zval **classVal;
-	    zend_hash_find(EG(class_table), "mage", sizeof("mage"),  (void **) &classVal);
-	    php_var_dump(classVal, 1 TSRMLS_CC);
 		return 1;
 	}
 	return 0;
@@ -26,35 +24,14 @@ int hasMagento(TSRMLS_D)
 
 char* getMagentoPath(size_t *slen TSRMLS_DC)
 {
-    zval **classVal;
-    char *result;
-    zval funcApp, funcFrontController, funcFullAction, retController, retApp, retAction;
-
-    ZVAL_STRING(&funcApp, "app", 0);
-
-    zend_hash_find(EG(class_table), "mage", sizeof("mage"), (void **) &classVal);
-
-	if(call_user_function(EG(function_table), classVal, &funcApp, &retApp, 0, NULL TSRMLS_CC) == FAILURE)
+	char* result;
+	zval retval;
+	//if(zend_eval_string("Mage::app()->getFrontController()->getFullActionName();", &retval, "graphdat magento") == FAILURE)
+	if(zend_eval_string("Mage::app()->getRequest()->getRequestedRouteName().'::'.Mage::app()->getRequest()->getRequestedControllerName().'::'.Mage::app()->getRequest()->getRequestedActionName()", &retval, "graphdat magento") == FAILURE)
 	{
 		return NULL;
 	}
-	// reference to the app
-	zval *pRetApp = &retApp;
-	ZVAL_STRING(&funcFrontController, "getfrontcontroller", 0);
-	if(call_user_function(EG(function_table), &pRetApp, &funcFrontController, &retController, 0, NULL TSRMLS_CC) == FAILURE)
-	{
-		return NULL;
-	}
-	// reference to the controller
-	zval *pRetController = &retController;
-	ZVAL_STRING(&funcFullAction, "getfullactionname", 0);
-	if(call_user_function(EG(function_table), &pRetController, &funcFullAction, &retAction, 0, NULL TSRMLS_CC) == FAILURE)
-	{
-		return NULL;
-	}
-
-    result = Z_STRVAL_P(&retAction);
-    *slen = Z_STRLEN_P(&retAction);
-
+	result = Z_STRVAL_P(&retval);
+    *slen = Z_STRLEN_P(&retval);
 	return result;
 }
